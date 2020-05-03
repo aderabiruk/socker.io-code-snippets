89ae49d6e45ceb5fe345a58cf7ac496e6feaddd9
Biruk Adera <aderabiruk@gmail.com>
2020-05-03 21:14:55 +0300

    Errors Prepared

---
+++ packages/api/src/errors/constants.ts
@@ -0,0 +1,19 @@
+export const ERROR_STATUS_CODES = {
+    BAD_INPUT_ERROR: 400,
+    UNAUTHORIZED_ERROR: 401,
+    FORBIDDEN_ERROR: 403,
+    NOT_FOUND_ERROR: 404,
+    INTERNAL_SERVER_ERROR: 500
+};
+
+export const ERROR_MESSAGES = {
+    /**
+     * User Error
+     */
+    USER_NOT_FOUND: "User not found!",
+
+    /**
+     * General Error
+     */
+    INTERNAL_SERVER_ERROR: "Internal Error: Something went wrong!",
+};
